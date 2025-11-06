a9b8da49ac8d1d261f4c87bb1ba8e73fe526d3a5
EisnerMarko <mareis01@easv365.dk>
2025-11-06 18:50:30 +0100

    fix vite config path for css js

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  base: '/eisner-2025-todo-TS-app-bde-main/',
   test: {
     environment: 'jsdom',
   },
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/eisner-2025-todo-TS-app-bde-main/',
+  build: {
+    outDir: 'dist',
+  },
+})
